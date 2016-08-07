e16dcc6fa5961207f9bc5a222f110f4c5910fb3f
Christian Stevens <stev1905@gmail.com>
2016-08-07 15:56:43 -0400

    guess increment +1 and guessed numbers display work

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function(){
 	
 	/*--- Display information modal box ---*/
@@ -11,7 +10,28 @@ $(document).ready(function(){
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
+  		
+  	//function newgame(){
+  	//	var randomnumber= Math.floor(Math.random() * 100 + 1);
+  	//	return randomnumber;
 
+  	//}
+   var values = [];
+  	$('#guessButton').on('click',function(i){
+  			//alert('This feature works');
+  			 values.push($('#userGuess').val()); // add value to array
+  			 var list= values.pop();
+             $('#guessList').append('<li> ' + list+ '</li>');
+             $('#userGuess').val('');
+             var value = parseInt($("#count").text(), 10) + 1;
+    		 $("#count").text(value);    
+
+  	});
+
+  		//$('.new').click(function(){
+  		//alert(newgame());
+  	//});
+  	
 });
 
 
