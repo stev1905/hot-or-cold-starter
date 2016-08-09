f441b5a470610dd956735862c3b78f8139016c33
Christian Stevens <stev1905@gmail.com>
2016-08-08 20:54:34 -0400

    Project complete with logic and number and empty input validation

--- js/app.js
+++ js/app.js
@@ -14,31 +14,72 @@ $(document).ready(function(){
     var values = [];
     var randomnumber= Math.floor(Math.random() * 100 + 1);
 
-  	function SaveGuess(){
-  			//alert('This feature works');
-  			 values.push($('#userGuess').val()); // add value to array
-  			 var list= values.pop();
-             $('#guessList').append('<li> ' + list+ '</li>');
-             $('#userGuess').val('');
-             var value = parseInt($("#count").text(), 10) + 1;
-    		 $("#count").text(value); 
-}  
  	function NewGame(){
+ 		$('#feedback').empty();
+ 		$('#userGuess').val('');
+  		$('#feedback').append('Make your Guess!');
   		$("#count").text('0');
   		$('#guessList').children().detach();
   		var randomnumber= Math.floor(Math.random() * 100 + 1);
   		return randomnumber;
+  		
 	}
+
 	function HotnColdMeter(){
-		userGuess = parseInt($('#userGuess').val());
-		var a= userGuess+5;
-		var b= userGuess-5;
-		if ((userGuess<=a)&&(userGuess>=b)){
-			alert('you are close');
+		userGuess=parseInt($('#userGuess').val());
+		var reallyhot_ceiling=randomnumber+5; 
+		var reallyhot_floor=randomnumber-5; 
+		var hot_ceiling=randomnumber+10;
+		var hot_floor=randomnumber-10;
+		var warm_ceiling=randomnumber+20;
+		var warm_floor=randomnumber-20;
+		var cold_ceiling=randomnumber+40;
+		var cold_floor=randomnumber-40;
+		var vcold_ceiling=randomnumber+70;
+		var vcold_floor=randomnumber-70;
+		var xcold_ceiling=randomnumber+70;
+		var xcold_floor=randomnumber-70;
+
+		if(userGuess===randomnumber){
+		 	$('#feedback').empty();
+		 	$('#feedback').append('You guessed Correctly!!!<br>Great Job!!');
 		}
-		else{
-			alert('not close');
+		else if((userGuess<=reallyhot_ceiling)&&(userGuess>=reallyhot_floor)){
+		 	//alert('you\'re very Hot!!!!');
+		 	$('#feedback').empty();
+		 	$('#feedback').append('you\'re very Hot!!!!');
 		}
+		else if((userGuess<=hot_ceiling)&&(userGuess>=hot_floor)){
+		 	//alert('you\'re getting Hot!!');
+		 	$('#feedback').empty();
+		 	$('#feedback').append('you\'re getting Hot!!');
+		}
+		else if((userGuess<=warm_ceiling)&&(userGuess>=warm_floor)){
+		 	//alert('you\'re getting Warm!');
+		 	$('#feedback').empty();
+		 	$('#feedback').append('you\'re getting Warm!');
+		}
+		else if((userGuess<=cold_ceiling)&&(userGuess>=cold_floor)){
+		 	//alert('you\'re Cold..');
+		 	$('#feedback').empty();
+		 	$('#feedback').append('you\'re Cold..');
+		}
+		else if((userGuess<=vcold_ceiling)&&(userGuess>=vcold_floor)){
+		 	//alert('you\'re very Cold...');
+		 	$('#feedback').empty();
+		 	$('#feedback').append('you\'re very Cold...');
+		}
+		else if((userGuess<=xcold_ceiling)&&(userGuess>=xcold_floor)){
+		 	//alert('Not even close bruh!!');
+		 	$('#feedback').empty();
+		 	$('#feedback').append('Not even close bruh!!');
+		}
+		 values.push($('#userGuess').val()); // add value to array
+  	     var list= values.pop();
+         $('#guessList').append('<li> ' + list+ '</li>');
+		 var value = parseInt($("#count").text(), 10) + 1;
+    	 $("#count").text(value); 
+		 $('#userGuess').val('');
 	}
 	$('.new').click(function(){
   		//alert('this button works too!!');
@@ -46,10 +87,18 @@ $(document).ready(function(){
   	});
 
     $('#guessButton').on('click',function(){
-    	alert(randomnumber);
-    	//return SaveGuess();
+    	var validate = $('#userGuess').val()  
+    	if(validate===""){							
+    		$('#userGuess').val('');
+    		alert('This field cannot be empty!');
+    		return false;
+    	}
+    	if((validate>100)||(validate<0)){  
+    		$('#userGuess').val('');
+    		alert('Please guess a value between 1-100');
+    		return false;
+    	}
     	return HotnColdMeter();
-
   	});
 });
 
